c07aca032feb0012017d85b6aeb28d84113f7499
Jiheui Lee <jiheuilee@Jiheuis-MacBook-Pro.local>
2022-02-06 16:06:41 +0900

    fixed longestPalidrome

--- jiheui/longestPalidrome.py
+++ jiheui/longestPalidrome.py
@@ -2,28 +2,28 @@ from itertools import combinations
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        word=[]
-        if len(s)==1: return s
-        elif len(s)==2:
-            if s[0]==s[1]: return s
-            else: return s[0]
-        elif len(set(s))==1:
-            return s
+        x,y = 0,0
+        s=s.replace("","@")
+        if len(list(set(s)))==1:
+            x,y=0,len(s)
+            pass
         else:
-            for i in range(1, len(s)-1):
-                add=1
-                while True:
-                    if add<=i and add<len(s)-i and s[i-add]==s[i+add]:
-                        word.append(s[(i-add):(i+add+1)])
-                        add+=1
-                    elif add<len(s)-i and s[i]==s[i+add]:
-                        word.append(s[i:i+add+1])
-                        add+=1
-                    else: break
-                i+=add
-            if len(word)>0: return max(word, key=len)
-            else: return ''
+            i, compare = 1,1
+            while i < len(s):
+                if (compare<=i) and (compare<=len(s)-i-1):
+                    if s[i-compare] != s[i+compare]:
+                        if s[i-compare]==s[i] and y-x<compare:
+                            x,y=i-compare,i
+                        elif s[i]==s[i+compare] and y-x<compare:
+                            x,y=i,i+compare
+                        i+=1
+                        compare=1
+                    else:
+                        if y-x<compare+compare:
+                            x,y=i-compare,i+compare
+                        compare+=1
+                else:
+                    i+=1
+                    compare=1
 
-
-
-
+        return s[x:y+1].replace("@","")
